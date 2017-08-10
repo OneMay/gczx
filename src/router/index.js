27dda22c102f27c71d9cfa0663006cf5c8765987
OneMay <1102304904@qq.com>
2017-08-10 15:16:56 +0800

    new

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import database from '@/components/database'
+//import canvas from '@/components/canvas'
+//import products from '@/components/products'
+//import detail from '@/components/the-product'
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: []
+})
